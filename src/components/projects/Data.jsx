b742822786a67212833e8783bff2ef921ee15209
Sergio Navarro <nachonavarro197@gmail.com>
2025-01-20 21:27:48 -0300

    concesionaria

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,5 +1,5 @@
 import Image1 from "../../assets/tutucas_img.jpg";
-import Image2 from "../../assets/pruebaimagen2.png";
+import Image2 from "../../assets/concesionaria.jpg";
 import Image3 from "../../assets/pruebaimagen3.png";
 import Image4 from "../../assets/pruebaimagen4.png";
 
@@ -7,18 +7,17 @@ export const Data = [
     {
         id: 1,
         image: Image1,
-        category: "React",
         title: "Tutucas e-commerce",
         link: "https://ecommerce-tutucas.vercel.app",
         descripcion: "Tienda Virtual de prueba realizada para practicar y demostrar el manejo de React, NextJS, TailwindCSS y Typescript, ademas de las implementaciones de librerias como shadcn. Backend hecho con Strapi"
     },
     {
         id: 2,
-        image: Image2,
-        category: "React", 
-        title: "Proyecto 2",
-        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Consequatur blanditiis, laboriosam quod aut sit laborum odio quaerat dolorum magni quasi voluptatibus quam.Ut suscipit mollitia quia fugiat.Nesciunt, quibusdam sint."
-    },
+        image: Image2, 
+        title: "Concesionaria",
+        link: "https://github.com/SergiNavarr/concesionaria",
+        descripcion: "Aplicación de escritorio hecha en Java para practicar y demostrar conocimientos en POO, MySQL y JPA. Permite hacer altas, bajas, consultas y modificaciones de automoviles en una base de datos"
+        },
     /*{
         id: 3,
         image: Image3,
